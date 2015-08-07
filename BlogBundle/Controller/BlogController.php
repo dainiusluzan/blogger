bc556ca53ff0661170694ab1864e6e5f14416ed6
axonmedia <hello@axonmedia.eu>
2015-08-07 21:22:39 +0300

    blogger testas

---
+++ BlogBundle/Controller/BlogController.php
@@ -0,0 +1,46 @@
+<?php
+// src/Blogger/BlogBundle/Controller/BlogController.php
+/**
+ * Created by PhpStorm.
+ * User: martynas
+ * Date: 2015.08.04
+ * Time: 23:19
+ */
+
+namespace Blogger\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Blog controller.
+ */
+class BlogController extends Controller
+{
+    /**
+     * Show a blog entry
+     */
+    public function showAction($url)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+//        $blog = $em->getRepository('BloggerBlogBundle:Blog')->find($url);
+        $blog = $em->getRepository('BloggerBlogBundle:Blog')->findOneByUrl($url);
+
+        if (!$blog) {
+//            print_r($em);
+            throw $this->createNotFoundException('Unable to find Blog post1.');
+        }
+
+        if (!$blog) {
+            throw $this->createNotFoundException('Unable to find Blog post2.');
+        }
+
+        $comments = $em->getRepository('BloggerBlogBundle:Comment')
+            ->getCommentsForBlog($blog->getId());
+
+        return $this->render('BloggerBlogBundle:Blog:show.html.twig', array(
+            'blog'      => $blog,
+            'comments'  => $comments
+        ));
+    }
+}
